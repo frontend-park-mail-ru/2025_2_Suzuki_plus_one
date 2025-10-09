569ace740420d2b2a14b72f69fe36b23cc779cbe
ivanmaryshev <ivan.maryshev.90@mail.ru>
2025-10-09 11:12:16 +0300

    fix: change outDir path

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
         },
     },
     build: {
-        outDir: path.resolve(__dirname,'dist'),
+        outDir: path.resolve(__dirname,'public/dist'),
         assetsDir: 'assets',
     },
     server: {
