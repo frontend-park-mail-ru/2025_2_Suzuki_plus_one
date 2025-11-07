c98084edbfc3ff08387b96b823162793495945e1
dinara <dinaragig03@gmail.com>
2025-11-07 13:06:33 +0300

    fix: change paths to api

--- src/shared/api/moviesApi.js
+++ src/shared/api/moviesApi.js
@@ -10,15 +10,22 @@ import { fetchWithErrorsHandling } from '@shared/utils/errorHandler';
  * @returns {Promise<Array<Object>>} Array of movie objects
  */
 export async function fetchMovies() {
-    return fetchWithErrorsHandling('/api/v1/movie/recommendations?limit=2', {
-        //   return fetchWithErrorsHandling('http://217.16.18.125/api/v1/movie/recommendations?limit=2', {
+    return fetchWithErrorsHandling('/api/v1/media/recommendations?type=movie&limit=2', {
+//    return fetchWithErrorsHandling('http://217.16.18.125/api/v1/media/recommendations?type=movie&limit=2', {
         method: 'GET',
     });
 }
 
 export async function fetchFilm(filmId) {
     return await fetchWithErrorsHandling(`/api/v1/media/${filmId}`, {
-        //   return await fetchWithErrorsHandling(`http://217.16.18.125/api/v1/media/${filmId}`, {
+//           return await fetchWithErrorsHandling(`http://217.16.18.125/api/v1/media/${filmId}`, {
+        method: 'GET',
+    });
+}
+
+export async function fetchStarsByFilmId(filmId) {
+    return await fetchWithErrorsHandling(`/api/v1/media/${filmId}/actor`, {
+ //          return await fetchWithErrorsHandling(`http://217.16.18.125/api/v1/media/${filmId}/actor`, {
         method: 'GET',
     });
 }
--- src/shared/api/starApi.js
+++ src/shared/api/starApi.js
@@ -1,8 +1,8 @@
 import { fetchWithErrorsHandling } from '@shared/utils/errorHandler';
 
 export async function fetchStar(starId) {
-    // return await fetchWithErrorsHandling(`http://217.16.18.125/api/v1/actor/${starId}`, {
-        return await fetchWithErrorsHandling(`/api/v1/actor/${starId}`, {
-            method: 'GET',
-        });
-    }
+    //    return await fetchWithErrorsHandling(`http://217.16.18.125/api/v1/actor/${starId}`, {
+    return await fetchWithErrorsHandling(`/api/v1/actor/${starId}`, {
+        method: 'GET',
+    });
+}
