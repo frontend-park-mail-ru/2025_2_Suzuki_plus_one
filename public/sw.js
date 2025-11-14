719546634b25b9b57b52bcc0c8a6765a80720b7e
ivanmaryshev <ivan.maryshev.90@mail.ru>
2025-11-14 15:54:51 +0300

    fix: update service worker

--- public/sw.js
+++ public/sw.js
@@ -63,8 +63,12 @@ const STATIC_ASSETS = [
 const API_ENDPOINTS = [
     '/api/v1/auth/signin',
     '/api/v1/auth/signup',
-    '/api/v1/auth/check',
-    '/api/movies',
+    '/api/v1/media',
+    '/api/v1/actor',
+    '/api/v1/auth/refresh',
+    '/api/v1/auth/signout',
+    '/api/v1/user',
+
 ];
 
 self.addEventListener('install', (event) => {
