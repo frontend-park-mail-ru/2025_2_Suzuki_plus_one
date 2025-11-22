0bf4596cf4c97cc53079134a93cfc2259d4241c7
ivanmaryshev <ivan.maryshev.90@mail.ru>
2025-11-22 21:06:58 +0300

    fix: player issues

--- src/widgets/Player/Player.js
+++ src/widgets/Player/Player.js
@@ -4,6 +4,8 @@ import { initPlayerControls } from './js/player-controls.js';
 import video from '@assets/videos/trailer.mp4';
 import poster from '@assets/images/poster.png';
 import { fetchTrailer } from '@shared/api/trailerApi.js';
+import RewindLeft from '@shared/assets/images/icons/circular-arrow-left.svg?raw';
+import RewindRight from '@shared/assets/images/icons/circular-arrow-right.svg?raw';
 
 class Player {
     #parent;
@@ -24,26 +26,31 @@ class Player {
         //     poster: film.poster,
         // });
 
-        const film = await fetchTrailer(this.#filmId);
-        const videoUrl = film.trailers && film.trailers.length > 0 
-                ? film.trailers[0] 
-                : null;
+        // const film = await fetchTrailer(this.#filmId);
+        // const videoUrl = film.trailers && film.trailers.length > 0 
+        //         ? film.trailers[0] 
+        //         : null;
 
-            const posterUrl = film.posters && film.posters.length > 0 
-                ? film.posters[0] 
-                : poster;
+        //     const posterUrl = film.posters && film.posters.length > 0 
+        //         ? film.posters[0] 
+        //         : poster;
 
-            if (!videoUrl) {
-                this.#parent.innerHTML = '<p style="text-align:center; color:red;">Trailer not available</p>';
-                return;
-            }
+        //     if (!videoUrl) {
+        //         this.#parent.innerHTML = '<p style="text-align:center; color:red;">Trailer not available</p>';
+        //         return;
+        //     }
 
-            this.#parent.innerHTML = template({
-                video: videoUrl,
-                poster: posterUrl,
+        //     this.#parent.innerHTML = template({
+        //         video: videoUrl,
+        //         poster: posterUrl,
+        //     });
+
+        this.#parent.innerHTML = template({
+             video,
+            poster,
+            rewindLeftIcon: RewindLeft,
+            rewindRightIcon: RewindRight,
             });
-
-        // this.#parent.innerHTML = template({ video, poster });
 
         requestAnimationFrame(() => {
             initPlayerControls();
