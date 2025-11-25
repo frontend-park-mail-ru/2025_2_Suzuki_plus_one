e4d8f0d26465ca78ebfb5eddf9b04a0acabb38a6
DinaraGil <dinaragig03@gmail.com>
2025-11-25 17:19:27 +0300

    fix: add posters

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -107,9 +107,9 @@ class Home {
             films = response.medias.map(film => ({
                 id: film.media_id,
                 title: film.title,
-  //              genres: film.genres ? film.genres.map(g => g.name).join(', ').toLowerCase() : '',
+                genres: film.genres ? film.genres.map(g => g.name).join(', ').toLowerCase() : '',
                 release_date: film.release_date.substr(0, 4),
- //               poster: film.posters[0],
+                poster: film.posters[0],
             }));
         }
         else {
