671a417cce58a4c62d9871a3c7eb64dc9168c640
sorrtory <62436169+sorrtory@users.noreply.github.com>
2025-12-12 14:37:34 +0300

    chore(payment): set redirect to manual

--- src/shared/api/paymentApi.js
+++ src/shared/api/paymentApi.js
@@ -15,7 +15,7 @@ export async function createNewPayment(data = {}) {
         },
         body: JSON.stringify(data),
         credentials: 'include',
-        redirect: 'follow',
+        redirect: 'manual',
     });
 
     console.log("resp", response)
