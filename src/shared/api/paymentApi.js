a75092cbf4619677bce483a7c93532bcc9abd63c
ivanmaryshev <ivan.maryshev.90@mail.ru>
2025-12-12 11:36:43 +0300

    try manual redirect

--- src/shared/api/paymentApi.js
+++ src/shared/api/paymentApi.js
@@ -18,21 +18,22 @@ export async function createNewPayment(data = {}) {
         redirect: 'follow',
     });
 
-    const location = response.headers.get('Location') || response.headers.get('location');
+    // const location = response.headers.get('Location') || response.headers.get('location');
 
-    if (location) {
-        window.location.href = location;
-        return;
-    }
+    // if (location) {
+    //     window.location.href = location;
+    //     return;
+    // }
 
-    let errorMessage = 'Redirect URL not found';
-    let errorData = null;
+    // let errorMessage = 'Redirect URL not found';
+    // let errorData = null;
 
-    try {
-        errorData = await response.json();
-        errorMessage = errorData.message || errorMessage;
-    } catch {
-    }
+    // try {
+    //     errorData = await response.json();
+    //     errorMessage = errorData.message || errorMessage;
+    // } catch {    
+    //     throw new Error(errorMessage);
+    // }
 
-    throw new Error(errorMessage);
+
 }
