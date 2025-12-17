5e77f51cdcbb7eb2299e4e0d695c31a0ee35bf2b
ivanmaryshev <ivan.maryshev.90@mail.ru>
2025-12-17 12:17:47 +0300

    fix: minor bug with likes dislikes

--- src/pages/SeriesPage/SeriesPage.js
+++ src/pages/SeriesPage/SeriesPage.js
@@ -288,7 +288,7 @@ class SeriesPage {
                     if (isCurrentlyLike && isCurrentlyDislike) {
                         await removeMediaReaction(mediaId);
                         await setMediaReaction(mediaId, 'like');
-                        this.#showToast('Removed from disliked and liked', 'success');
+                        this.#showToast('Liked', 'success');
                     } else if (isCurrentlyLike && !isCurrentlyDislike) {
                         await removeMediaReaction(mediaId);
                         this.#showToast('Removed from  Liked!', 'success');
